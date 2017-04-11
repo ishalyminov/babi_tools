c2de358bec48954342a77d3fb724fa8a541bf132
Igor Shalyminov <ishalyminov@gmail.com>
2017-04-11 17:43:04 +0100

    random shuffle fixed

--- random_shuffle_dataset.py
+++ random_shuffle_dataset.py
@@ -9,8 +9,8 @@ random.seed(273)
 
 
 def main(in_data_root):
-    train_set, dev_set, test_set = load_dataset(in_data_root, TASK_ID)
-    index = range(len(train_set) + len(dev_set) + len(test_set))
+    train_set, dev_set, test_set, testoov_set = load_dataset(in_data_root, TASK_ID)
+    index = range(len(train_set) + len(dev_set) + len(test_set) + len(testoov_set))
     random.shuffle(index)
     return index
 
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage: {} <babi root>'.format(path.basename(__file__))
         exit()
-    print ';'.join(map(str, main(sys.argv[1])))
+    print ' '.join(map(str, main(sys.argv[1])))
