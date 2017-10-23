f3e0091d56f70c517fa95aad660dac1655b9258a
Igor Shalyminov <ishalyminov@gmail.com>
2017-10-23 21:04:00 +0100

    dstc6 to babi conversion script added

---
+++ dstc6_to_babi.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+def main(in_src_file, in_dst_file):
+    with open(in_src_file) as dialogues_in:
+        dialogues = json.load(dialogues_in)
+    processed_dialogues = []
+    for dialogue in dialogues:
+        utterances = dialogue['utterances'][:]
+        utterances.append(dialogue['answer']['utterance'])
+        processed_dialogues.append(utterances)
+    with open(in_dst_file, 'w') as dialogues_out:
+        for dialogue in processed_dialogues:
+            for turn, index in enumerate(xrange(0, len(dialogue) - 2, 2)):
+                usr, sys = dialogue[index: index + 2]
+                print >>dialogues_out, '{} {}\t{}'.format(turn + 1, usr, sys)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'Usage: {} <dstc6 dialogues file> <result file>'.format(
+            os.path.basename(__file__)
+        )
+        exit()
+    main(sys.argv[1], sys.argv[2])
